f998b1ea96175f3ff8d447c49ad39c1c646ac048
firmslan <firas.khaldi1@gmail.com>
2022-12-19 18:34:26 -0500

    finished quote project but baba HAS to delete all

---
+++ quote generator/script.js
@@ -0,0 +1,8 @@
+const quoteContainer = document.getElementById('quote-container')
+const quoteText = document.getElementById('quote')
+const authorText = document.getElementById('author')
+const twitterBtn = document.getElementById('twitter')
+const newQuoteBtn = document.getElementById('new-quote')
+const loader = document.getElementById('loader')
+const apiUrl = 'https://type.fit/api/quotes'
+//const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
