b1bfc09399bbf3b096fccc328413209b72723d79
firmslan <firas.khaldi1@gmail.com>
2023-01-05 19:46:27 -0500

    finished project

---
+++ animated-navigation/script.js
@@ -0,0 +1,38 @@
+const menuBars = document.getElementById('menu-bars')
+const overlay = document.getElementById('overlay')
+const nav1 = document.getElementById('nav-1')
+const nav2 = document.getElementById('nav-2')
+const nav3 = document.getElementById('nav-3')
+const nav4 = document.getElementById('nav-4')
+const nav5 = document.getElementById('nav-5')
+const navItems = [nav1, nav2, nav3, nav4, nav5]
+
+function navAnimation(direction1, direction2){
+    navItems.forEach((nav, index)=>{
+        nav.classList.replace(`slide-${direction1}-${index + 1}`,  `slide-${direction2}-${index + 1}`)
+    })
+}
+function toggleNav(){
+    //toggle menu bars
+    menuBars.classList.toggle('change')
+    //togle menu active
+    overlay.classList.toggle('overlay-active')
+    if(overlay.classList.contains('overlay-active')){
+        overlay.classList.replace('overlay-slide-left', 'overlay-slide-right')
+        //animate in
+        navAnimation('out', 'in')
+
+    }else{
+        //animate out
+        overlay.classList.replace('overlay-slide-right', 'overlay-slide-left')
+        /////////////////////////////////////
+        navAnimation('in', 'out')
+    }
+}
+
+//Event listeners
+
+menuBars.addEventListener('click', toggleNav)
+navItems.forEach((nav)=>{
+    nav.addEventListener('click', toggleNav)
+})
